3923a902de26de6ae488d23f74fa97f8bd9fa2ba
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-04 16:26:12 +0300

    add-Rates

--- src/reduxState/operations.js
+++ src/reduxState/operations.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { exchangeCurrency } from 'service/exchangeAPI';
+import { exchangeCurrency, latestRates } from 'service/exchangeAPI';
 import { getUserInfo } from 'service/opencagedataApi';
 
 export const fetchBaseCurrency = createAsyncThunk(
@@ -26,6 +26,18 @@ export const fetchExchangeCurrency = createAsyncThunk(
     try {
       const data = await exchangeCurrency(credentials);
       console.log(data);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const fetchLastSumbols = createAsyncThunk(
+  'currency/fetchLastSumbols',
+  async (baseCurrency, thunkAPI) => {
+    try {
+      const data = await latestRates(baseCurrency);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
--- src/reduxState/selectors.js
+++ src/reduxState/selectors.js
@@ -1,4 +1,16 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectCurrency = state => state.currency.baseCurrency;
 export const selectExchange = state => state.currency.exchangeInfo;
 export const selectError = state => state.currency.isError;
 export const selectLoading = state => state.currency.isLoading;
+export const selectRates = state => state.currency.rates;
+
+export const selectFilterRates = createSelector(
+  [selectCurrency, selectRates],
+  (baseCurrency, rates) => {
+    return rates
+      .filter(([key]) => key !== baseCurrency)
+      .map(([key, value]) => ({ key, value: (1 / value).toFixed(2) }));
+  },
+);
